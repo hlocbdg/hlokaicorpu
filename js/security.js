1d7e6c1896bd6717cffc8d6e31b831bc30ee4cbd
hlocbdg <hloc.bdg@gmail.com>
2026-02-24 15:46:24 +0700

    Update security.js

--- js/security.js
+++ js/security.js
@@ -299,4 +299,19 @@ window.addEventListener("resize", function(){
   });
 });
 
+
+})();
+
+/* =========================
+   AUTH GUARD
+========================= */
+
+(function(){
+
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if(isLoggedIn !== "true"){
+        window.location.href = "index.html";
+    }
+
 })();
