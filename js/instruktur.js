e018d7d8c76f8b525b1abc4fc40c651e2210887d
hlocbdg <hloc.bdg@gmail.com>
2026-02-24 15:45:32 +0700

    Update instruktur.js

--- js/instruktur.js
+++ js/instruktur.js
@@ -426,4 +426,17 @@ wrapper.addEventListener("change", function(){
 });
 
 }
+/* =========================
+   AUTH GUARD
+========================= */
+
+(function(){
+
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if(isLoggedIn !== "true"){
+        window.location.href = "index.html";
+    }
+
+})();
 
